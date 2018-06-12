d8619deb2258c4b609baed28d81ca59f28cb96eb
weasny <weasny@weasnydeMacBook-Pro.local>
2018-06-12 17:14:44 +0800

    append pep8 formation

--- web01/client.py
+++ web01/client.py
@@ -13,10 +13,10 @@ print('本机IP 和 port : {}, {}'.format(ip, port))
 
 http_request = 'GET / HTTP/1.1\r\nhost:{}\r\n\r\n'.format(host)
 req = http_request.encode('utf-8')
-print ('请求', req)
+print('请求', req)
 s.send(req)
 
 # receive data
 response = s.recv(1024)
-print ('响应', response)
-print ('str 响应', response.decode('utf-8'))
+print('响应', response)
+print('str 响应', response.decode('utf-8'))

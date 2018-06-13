826b3b08aa30f57aa8c0ae75189ccb3bf52bac88
weasny <weasny@weasnydeMacBook-Pro.local>
2018-06-13 18:30:34 +0800

    append homework02

---
+++ web02/homework02.py
@@ -0,0 +1,25 @@
+# 2017/02/18
+# 作业 2
+# ========
+#
+#
+# 请直接在我的代码中更改/添加, 不要新建别的文件
+
+
+# 定义我们的 log 函数
+def log(*args, **kwargs):
+    print(*args, **kwargs)
+
+
+# 作业 2.1
+#
+# 实现函数
+def path_with_query(path, query):
+    '''
+    path 是一个字符串
+    query 是一个字典
+
+    返回一个拼接后的 url
+    详情请看下方测试函数
+    '''
+    pass

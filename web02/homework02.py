212ab06dbd9895b195bb7262527b72bba5b72210
weasny <weasny@weasnydeMacBook-Pro.local>
2018-06-14 18:33:17 +0800

    unfinished homework02

--- web02/homework02.py
+++ web02/homework02.py
@@ -22,4 +22,92 @@ def path_with_query(path, query):
     返回一个拼接后的 url
     详情请看下方测试函数
     '''
-    pass
+    query_str = ''
+    for q in query:
+        value = str(q) + '=' + str(query[q]) + '&'
+        log(value)
+        query_str += value
+    return path + '?' + query_str[:-1]
+
+
+def test_path_with_query():
+    path = '/'
+    query = {
+        'name': 'gua',
+        'height': 169,
+    }
+    expected = [
+        '/?name=gua&height=169',
+        '/?height=169&name=gua',
+    ]
+    assert path_with_query(path, query) in expected
+
+
+# 作业 2.2
+#
+# 为作业1 的 get 函数增加一个参数 query
+# query 是字典
+
+
+# 作业 2.3
+#
+# 实现函数
+def header_from_dict(headers):
+    '''
+    headers 是一个字典
+    范例如下
+    对于
+    {
+      'Content-Type': 'text/html',
+      'Content-Length': 127,
+    }
+    返回如下 str
+    'Content-Type: text/html\r\nContent-Length: 127\r\n'
+    '''
+    headers_str = ''
+    for h in headers:
+        headers_str += str(h) + ': ' + str(headers[h]) + '\r\n'
+    print(headers_str)
+    return headers_str
+
+
+# 作业 2.4
+#
+# 为作业 2.3 写测试
+def test_header_from_dict():
+    headers = {
+        'Connection': 'Keep-Alive', 'Content-Type': 'text/html',
+    }
+    respected = [
+        'Connection: Keep-Alive\r\nContent-Type: text/html\r\n',
+        'Content-Type: text/html\r\nConnection: Keep-Alive\r\n',
+    ]
+    assert header_from_dict(headers) in respected
+
+
+# 作业 2.5
+#
+"""
+豆瓣电影 Top250 页面链接如下
+https://movie.douban.com/top250
+我们的 client_ssl.py 已经可以获取 https 的内容了
+这页一共有 25 个条目
+
+所以现在的程序就只剩下了解析 HTML
+
+请观察页面的规律，解析出
+1，电影名
+2，分数
+3，评价人数
+4，引用语（比如第一部肖申克的救赎中的「希望让人自由。」）
+
+解析方式可以用任意手段，如果你没有想法，用字符串查找匹配比较好(find 特征字符串加切片)
+"""
+
+
+def crawl_douban():
+    url = 'https://movie.douban.com/top250'
+
+
+if __name__ == '__main__':
+    test_header_from_dict()

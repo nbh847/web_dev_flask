c09d36cfe28324ba57d3b860b59b3f96836df10a
weasny <weasny@weasnydeMacBook-Pro.local>
2018-06-20 14:56:48 +0800

    save file

--- web03/routes.py
+++ web03/routes.py
@@ -2,7 +2,6 @@ from utils import log
 from models import Message
 from models import User
 
-
 message_list = []
 
 
@@ -40,3 +39,61 @@ def route_login(request):
         result = ''
     body = template('login.html')
     body = body.replace('{{result}}', result)
+    r = header + '\r\n' + body
+    return r.encode(encoding='utf-8')
+
+
+def route_register(request):
+    header = 'HTTP/1.1 210 VERY OK\r\nContent-Type: text/html\r\n'
+    if request.method == 'POST':
+        form = request.form()
+        u = User.new(form)
+        if u.validate_register():
+            u.save()
+            result = '注册成功<br> <pre>{}</pre>'.format(User.all())
+        else:
+            request = '用户名或者密码长度必须大于2'
+    else:
+        result = ''
+    body = template('register.html')
+    body = body.replace('{{result}}', result)
+    r = header + '\r\n' + body
+    return r.encode('utf-8')
+
+
+def route_message(request):
+    """
+    主页的处理函数, 返回主页的响应
+    """
+    log('本次请求的 method', request.method)
+    if request.method == 'POST':
+        form = request.form()
+        msg = Message.new(form)
+        log('post', form)
+        message_list.append(msg)
+    header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n'
+    body = template('html_basic.html')
+    msgs = '<br>'.join([str(m) for m in message_list])
+    body = body.replace('{{messages}}', msgs)
+    r = header + '\r\n' + body
+    return r.encode(encoding='utf-8')
+
+
+def route_static(request):
+    '''
+    静态资源的处理函数, 读取图片并生成响应返回
+    '''
+    filename = request.query.get('file', 'doge.gif')
+    path = 'static/' + filename
+    with open(path, 'rb') as f:
+        header = b'HTTP1.1 200 OK\r\nContent-Type: image/gif\r\n\r\n'
+        img = header + f.read()
+        return img
+
+
+route_list = {
+    '/': route_index,
+    '/login': route_login,
+    '/register': route_register,
+    '/messages': route_message,
+}

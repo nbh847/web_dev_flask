0e7272fdaaed6e8c38dd0579600d881cc432924a
binhui <binhui.ni@yourdream.cc>
2018-07-04 22:36:03 +0800

    save files

--- web04/routes.py
+++ web04/routes.py
@@ -51,5 +51,97 @@ def response_with_headers(headers):
     return header
 
 
-if __name__ == '__main__':
-    print(random_str())
+def route_login(request):
+    """
+    登录页面的路由函数
+    """
+    headers = {
+        'Content-Type': 'text/html',
+        # 'Set-Cookie': 'height=169; gua=1; pwd=2; Path=/',
+    }
+    # log('login, headers', request.headers)
+    log('login, cookies', request.cookies)
+    username = current_user(request)
+    if request.method == 'POST':
+        form = request.form()
+        u = User.new(form)
+        if u.validate_login():
+            session_id = random_str()
+            session[session_id] = u.username
+            headers['Set-Cookie'] = 'user={}'.format(session_id)
+            result = '登录成功'
+        else:
+            result = '用户名或者密码错误'
+    else:
+        result = ''
+    body = template('login.html')
+    body = body.replace('{{result}}', result)
+    body = body.replace('{{username}}', username)
+    header = response_with_headers(headers)
+    r = header + '\r\n' + body
+    # log('login', r)
+    return r.encode(encoding='utf-8')
+
+
+def route_register(request):
+    """
+    注册页面的路由函数
+    """
+    header = 'HTTP/1.x 210 VERY OK\r\nContent-Type: text/html\r\n'
+    if request.method == 'POST':
+        form = request.form()
+        u = User.new(form)
+        if u.validate_register():
+            u.save()
+            result = '注册成功<br> <pre>{}</pre>'.format(User.all())
+        else:
+            result = '用户名或者密码长度必须大于2'
+    else:
+        result = ''
+    body = template('register.html')
+    body = body.replace('{{result}}', result)
+    r = header + '\r\n' + body
+    return r.encode(encoding='utf-8')
+
+
+def route_message(request):
+    """
+    消息页面的路由函数
+    """
+    log('本次请求的 method', request.method)
+    if request.method == 'POST':
+        form = request.form()
+        msg = Message.new(form)
+        log('post', form)
+        message_list.append(msg)
+        # 应该在这里保存 message_list
+    header = 'HTTP/1.x 200 OK\r\nContent-Type: text/html\r\n'
+    # body = '<h1>消息版</h1>'
+    body = template('html_basic.html')
+    msgs = '<br>'.join([str(m) for m in message_list])
+    body = body.replace('{{messages}}', msgs)
+    r = header + '\r\n' + body
+    return r.encode(encoding='utf-8')
+
+
+def route_static(request):
+    """
+    静态资源的处理函数, 读取图片并生成响应返回
+    """
+    filename = request.query.get('file', 'doge.gif')
+    path = 'static/' + filename
+    with open(path, 'rb') as f:
+        header = b'HTTP/1.x 200 OK\r\nContent-Type: image/gif\r\n\r\n'
+        img = header + f.read()
+        return img
+
+
+# 路由字典
+# key 是路由(路由就是 path)
+# value 是路由处理函数(就是响应)
+route_dict = {
+    '/': route_index,
+    '/login': route_login,
+    '/register': route_register,
+    '/messages': route_message,
+}

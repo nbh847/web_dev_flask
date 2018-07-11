bf2d3c77c1300fd0be0d9a304dd78ed0dcd666aa
binhui <binhui.ni@yourdream.cc>
2018-07-11 22:18:34 +0800

    save files

--- web04/models.py
+++ web04/models.py
@@ -29,6 +29,7 @@ class Model(object):
     user = User()
     user.db_path() 返回 User.txt
     """
+
     @classmethod
     def db_path(cls):
         """
@@ -93,14 +94,17 @@ class Model(object):
         path = self.db_path()
         save(l, path)
 
+
 class User(Model):
     """
     User 是一个保存用户数据的 model
     现在只有两个属性 username 和 password
     """
+
     def __init__(self, form):
         self.username = form.get('username', '')
         self.password = form.get('password', '')
+        self.note = form.get('note', '')
 
     def validate_login(self):
         u = User.find_by(username=self.username)
@@ -114,6 +118,7 @@ class Message(Model):
     """
     Message 是用来保存留言的 model
     """
+
     def __init__(self, form):
         self.author = form.get('author', '')
         self.message = form.get('message', '')

af2d059828fcbc72baaa3dc645992d200bb43c77
binhui <binhui.ni@yourdream.cc>
2018-07-17 21:51:05 +0800

    save files

--- web04/routes.py
+++ web04/routes.py
@@ -45,10 +45,25 @@ def route_index(request):
     return r.encode('utf-8')
 
 
-def response_with_headers(headers):
-    header = 'HTTP/1.x 210 VERY OK\r\n'
+def response_with_headers(headers, status_code):
+    header = 'HTTP/1.x {} VERY OK\r\n'.format(status_code)
     header += ''.join(['{}: {}\r\n'.format(k, v) for k, v in headers.items()])
     return header
+
+
+def redirect(url):
+    """
+    浏览器在收到 302 响应的时候
+    会自动在 HTTP header 里面找 Location 字段并获取一个 url
+    然后自动请求新的 url
+    """
+    headers = {
+        'Location': url,
+    }
+    # 增加 Location 字段并生成 HTTP 响应返回
+    # 注意, 没有 HTTP body 部分
+    r = response_with_headers(headers, 302) + '\r\n'
+    return r.encode('utf-8')
 
 
 def route_profile(request):

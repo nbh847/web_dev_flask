316162a89fddd5ca199da4c59999da807e78a946
binhui <binhui.ni@yourdream.cc>
2018-08-08 20:59:17 +0800

    save files

--- web04/utils.py
+++ web04/utils.py
@@ -1,4 +1,7 @@
 import time
+import os
+
+from jinja2 import Environment, FileSystemLoader
 
 
 def log(*args, **kwargs):
@@ -7,4 +10,25 @@ def log(*args, **kwargs):
     format = '%Y/%m/%d %H:%M:%S'
     value = time.localtime(int(time.time()))
     dt = time.strftime(format, value)
-    print(dt, *args, **kwargs)
+    with open('log.gua.txt', 'a', encoding='utf-8') as f:
+        f.write(f.read())
+        print(dt, *args, file=f, **kwargs)
+
+
+# __file__ 就是本文件的名字
+# 得到用于加载模板的目录
+# os.path.dirname(__file__) 表示本文件所在的目录
+path = '{}/templates/'.format(os.path.dirname(__file__))
+# 创建一个加载器, jinja2 会从这个目录中加载模板
+loader = FileSystemLoader(path)
+# 用加载器创建一个环境, 有了它才能读取模板文件
+env = Environment(loader=loader)
+
+
+def template(path, **kwargs):
+    """
+    本函数接受一个路径和一系列参数
+    读取模板并渲染返回
+    """
+    t = env.get_template(path)
+    return t.render(**kwargs)
